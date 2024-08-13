dbd3aa19cd3a13c0caa61aa868edb75111a459f9
toanthanhtu <toantu3005@gmail.com>
2024-08-13 10:43:36 +1000

    complete 5.17 - 5.23

--- cypress/e2e/blogs_app.cy.js
+++ cypress/e2e/blogs_app.cy.js
@@ -1,7 +1,9 @@
 describe('Blog app', function () {
   beforeEach(function () {
+    // empty the database
     cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
 
+    // create a new user
     const user = {
       name: 'John Smith',
       username: 'john',
@@ -12,7 +14,7 @@ describe('Blog app', function () {
     cy.visit('')
   })
 
-  it('front page opened to log in page', function () {
+  it('front page opened to log in form', function () {
     cy.contains('log in to application')
     cy.contains('username')
     cy.contains('password')
@@ -35,7 +37,7 @@ describe('Blog app', function () {
     cy.get('.error')
       .contains('wrong username or password')
       .should('contain', 'wrong username or password')
-      .and('have.css', 'color', 'rgb(255, 0, 0)')
+      .and('have.css', 'color', 'rgb(255, 0, 0)') // notification is displayed in red
       .and('have.css', 'border-style', 'solid')
 
     cy.get('html').should('not.contain', 'John Smith logged in')
@@ -53,7 +55,21 @@ describe('Blog app', function () {
       cy.get('#author').type('Jack Daniels')
       cy.get('#url').type('https://fakeurl.com')
       cy.get('.create-button').click()
-      cy.contains('a blog created by cypress')
+      cy.contains('a blog created by cypress - Jack Daniels')
+    })
+
+    it('user can create a blog and delete that blog', function () {
+      cy.contains('create new blog').click()
+      cy.get('#title').type('a blog created by cypress')
+      cy.get('#author').type('Jack Daniels')
+      cy.get('#url').type('https://fakeurl.com')
+      cy.get('.create-button').click()
+      cy.contains('a blog created by cypress - Jack Daniels')
+
+      cy.contains('view').click()
+      cy.contains('remove').click()
+      cy.contains('blog a blog created by cypress by Jack Daniels removed')
+      cy.contains('a blog created by cypress - Jack Daniels').should('not.exist')
     })
 
     describe('and a blog exists', function () {
@@ -76,7 +92,10 @@ describe('Blog app', function () {
 
     describe('and several blogs exist', function () {
       beforeEach(function () {
+        // login with John Smith
         cy.login({ username: 'john', password: 'smith' })
+
+        // create blogs with john user
         cy.createBlog({
           title: 'First Blog by Cypress',
           author: 'John Cy',
@@ -104,13 +123,71 @@ describe('Blog app', function () {
         cy.contains('Second Blog by Cypress')
           .contains('like')
           .as('likeButton')
-        
+
         // click like twice
         cy.get('@likeButton').click()
         cy.get('@likeButton').click()
 
         cy.contains('Second Blog by Cypress')
           .contains('likes 2')
+      })
+
+      it('other user cannot see remove button on john\'s blogs', function () {
+        cy.contains('logout').click()
+
+        // create and login with a new user
+        const user = {
+          name: 'Adam Ling',
+          username: 'adam',
+          password: 'ling'
+        }
+        cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
+
+        cy.get('#username').type('adam')
+        cy.get('#password').type('ling')
+        cy.get('#login-button').click()
+
+        cy.contains('view').click()
+        cy.contains('remove').should('not.exist')
+      })
+
+      it.only('blogs are ordered by likes, most liked first', function () {
+        // find and click view to expand the second blog
+        cy.contains('Second Blog by Cypress')
+          .contains('view')
+          .click()
+
+        // find the like button and save it with 'as'
+        cy.contains('Second Blog by Cypress')
+          .contains('like')
+          .as('likeButton')
+
+        // click like twice
+        cy.get('@likeButton').click()
+        cy.get('@likeButton').click()
+
+        cy.contains('hide').click()
+
+        // find and click view to expand the third blog
+        cy.contains('Third Blog by Cypress')
+          .contains('view')
+          .click()
+
+        // find the like button and save it with 'as'
+        cy.contains('Third Blog by Cypress')
+          .contains('like')
+          .as('likeButton')
+
+        // click like 3 times
+        cy.get('@likeButton').click()
+        cy.get('@likeButton').click()
+        cy.get('@likeButton').click()
+
+        cy.contains('hide').click()
+
+        cy.get('.blog').eq(0).should('contain', 'Third Blog by Cypress')
+        cy.get('.blog').eq(1).should('contain', 'Second Blog by Cypress')
+        cy.get('.blog').eq(2).should('contain', 'First Blog by Cypress')
       })
     })
   })
